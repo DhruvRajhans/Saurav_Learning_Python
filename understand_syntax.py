aedf5708bc7e2db148f28cf23c5094ec2dc0b206
dev-srajhans <dev.srajhans@gmail.com>
2026-02-27 23:21:21 +0530

    27-02-2026

---
+++ understand_syntax.py
@@ -0,0 +1,27 @@
+print("Hello world!")
+print(3+4)
+
+# This is comand in python 
+
+# If Statment
+if(1):
+    print("This is true")
+else:
+    print("This is false")
+
+if(10 == 10):
+    print("Yes its really 10")
+else:
+    print("Its not 10")
+
+
+if(5==2):
+    print("5 is equal than 2")
+elif(5<2):
+    print("No 5 is not greater")
+
+if(0):
+    print("yes its zero")
+else:
+    if 1:
+        print("Yes its one")
---
+++ variables_and_data_types.py
@@ -0,0 +1,8 @@
+# we store data in memory location 
+age = 24 #integer
+name = "Harry" #string
+cgpa = 10.10 #float
+
+print("age: ",age)
+print("name: ",name)
+ 
